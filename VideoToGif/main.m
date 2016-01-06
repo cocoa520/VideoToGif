2230bef8c2f5d7d8bcbf7d7d5d2a1ecadeb60df6
maccoder <1499934954@qq.com>
2016-01-06 20:56:19 +0800

    Initial Commit

---
+++ VideoToGif/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  VideoToGif
+//
+//  Created by liping  on 16-1-6.
+//  Copyright (c) 2016年 SmartisanMac. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
